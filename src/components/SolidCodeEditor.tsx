c5e03f2ab3d8ab3fb9e2231575fd16ee11e81206
R. Rajesh Jeba Anbiah <4907427+rrjanbiah@users.noreply.github.com>
2022-04-10 04:23:20 +0530

    Handled and shown React syntax errors

--- src/components/SolidCodeEditor.tsx
+++ src/components/SolidCodeEditor.tsx
@@ -1,10 +1,61 @@
-import { Component } from "solid-js";
+import { Component, createEffect, onCleanup, onMount } from "solid-js";
+import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
+import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
+import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
+import * as monaco from 'monaco-editor';
+
+(window as any).MonacoEnvironment = {
+    getWorker(_moduleId: unknown, label: string) {
+        switch (label) {
+            case 'css':
+                return new cssWorker();
+            case 'typescript':
+            case 'javascript':
+                return new tsWorker();
+            default:
+                return new editorWorker();
+        }
+    },
+};
 
 const SolidCodeEditor: Component = (props) => {
-    return(
-        <textarea rows="20" cols="20"
-            onInput={(evt) => props.setSolidCode(evt.currentTarget.value)}
-        >{props.code}</textarea>
+    let editorRef!: HTMLDivElement;
+    let editor: monaco.editor.IStandaloneCodeEditor;
+    onMount(() => {
+        monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
+            noSemanticValidation: true,
+            noSyntaxValidation: true,
+        });
+        editor = monaco.editor.create(editorRef, {
+            value: props.code(),
+            language: 'typescript',
+            //lineNumbers: 'off',
+            roundedSelection: false,
+            scrollBeyondLastLine: false,
+            readOnly: true,
+            //theme: 'vs-dark',
+            minimap: { enabled: false },
+            lightbulb: { enabled: false },
+            hover: { enabled: false },
+            quickSuggestions: false,
+            //model: null,
+        });
+        editor.onDidChangeModelContent(() => {
+            console.log(editor.getValue());
+            props.setSolidCode(editor.getValue());
+        });
+    });
+    onCleanup(() => editor?.dispose());
+
+    return (
+        <>
+            <span>Monaco (not works):</span>
+            <div ref={editorRef} class="h-screen"></div>
+            <span>Textarea (works):</span>
+            <textarea cols="20" rows="10" class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                onInput={(evt) => props.setSolidCode(evt.currentTarget.value)}
+            >{props.code}</textarea>
+        </>
     );
 };
 
