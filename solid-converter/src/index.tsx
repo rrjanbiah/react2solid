66a8863158838a97ed9a6765702f707cec9ed98b
R. Rajesh Jeba Anbiah <4907427+rrjanbiah@users.noreply.github.com>
2022-04-09 14:55:18 +0530

    Restarted converter using Babel and SolidJS using solidjs/templates/ts-windicss 332b634e7698dc2165f6481ca296e92e3f066892

---
+++ solid-converter/src/index.tsx
@@ -0,0 +1,7 @@
+/* @refresh reload */
+import 'windi.css';
+import { render } from 'solid-js/web';
+
+import App from './App';
+
+render(() => <App />, document.getElementById('root') as HTMLElement);
---
+++ solid-converter/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
+import WindiCSS from 'vite-plugin-windicss';
+
+export default defineConfig({
+  plugins: [
+    solidPlugin(),
+    WindiCSS({
+      scan: {
+        fileExtensions: ['html', 'js', 'ts', 'jsx', 'tsx'],
+      },
+    }),
+  ],
+  build: {
+    target: 'esnext',
+    polyfillDynamicImport: false,
+  },
+});
