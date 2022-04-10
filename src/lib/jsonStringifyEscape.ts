cb6d04755296d786b5c26814019423ebed92f4bc
R. Rajesh Jeba Anbiah <4907427+rrjanbiah@users.noreply.github.com>
2022-04-10 22:14:22 +0530

    JSON.stringify with escape characters break JSON.parse. Created a helper for that

--- src/lib/jsonStringifyEscape.ts
+++ src/lib/jsonStringifyEscape.ts
@@ -1,5 +1,6 @@
 // https://stackoverflow.com/questions/4253367/how-to-escape-a-json-string-containing-newline-characters-using-javascript/14137856#14137856
 // Unfortunately, JSON.stringify() doesn't escape newlines in strings, hence JSON.parse() will fail
+// pass it as a second param to JSON.stringify()
 export function jsonStringifyEscape(key: any, val: any) {
     if (typeof (val) != "string") {
         return val;
